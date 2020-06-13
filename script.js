05bac3ea8317d53bcfc64f0470e6a71b1505312b
Christopher Holmes <holmes.chris94@yahoo.com>
2020-06-12 22:06:12 -0400

    This is my fourth commit

--- script.js
+++ script.js
@@ -4,37 +4,54 @@ var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = ["`", "~", "!", "#", "$", "%", "^", "&", "*", "(", ")", "+", ",", "-", "/", ":", ";", "?", ">", "@", "[", "]", "{", "}", "|"];
-
+var passwordOptions = []
+var password = []
 
 
 function generatePassword() {
   password = "";
+
   //Prompt the user to choose number of characters
   var numofChar = prompt("How many characters do you want in your password (must be between 8 and 128)?");
-  
+
   //Will revert back to original setup if password is too short
   if (numofChar < 8) {
     alert("Password must be at least 8 characters long");
   }
   //If password length meets criteria, then we can confirm what kinds of characters the user wants to use
-  if (numofChar >= 8 && numofChar <= 128) {
+  else if (numofChar > 128) {
+
+  } else {
     var askUpper = confirm("Would you like uppercase letters in your password?");
+    if (askUpper) { passwordOptions.push(upperCase) }
+
     var askLower = confirm("Would you like lowercase letters in your password?");
+    if (askLower) { passwordOptions.push(lowerCase) }
+
     var askNumbers = confirm("Would you like numbers in your password?");
+    if (askNumbers) { passwordOptions.push(askNumbers) }
+
     var askSpecialChar = confirm("Would you like special characters in you password?");
+    if (askSpecialChar) { passwordOptions.push(specialChar) }
+  }
+
+
+  var condenseArray = passwordOptions.flat()
+
+  console.log(condenseArray)
+
+  //For Loop
+
   
-  } else{
-    alert("You must select at least one character type.");
-  }
 
 
-  if (askUpper) {
-    password += pass
-    
-  }
+
+
+
 
   return "This is the password"
 }
+
 
 // Write password to the #password input
 function writePassword() {
